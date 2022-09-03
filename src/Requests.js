eafd837393e87f4ebafd36d1c617768dbd14cbaf
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-03 23:43:02 +0500

    API Call & Main Page Setup

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = "872dd2e6671a1a5a6bec2347ce4259cb";
+
+const requests = {
+  requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+  requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+  requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+  requestHorror: `https://api.themoviedb.org/3/search/company?api_key=${key}&query=horror&page=1`,
+  requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+};
+
+export default requests;
