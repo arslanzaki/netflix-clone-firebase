74371372e195978ee7da013221d457cb5be7bfc5
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-09-05 11:03:44 +0500

    Signup Page Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Account from "./pages/Account";
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between p-4 z-[100] absolute w-full">
-        <h1 className="text-4xl font-bold text-red-500 cursor-pointer">NETFLIX</h1>
-        <div>
-            <button className="text-white mr-4">Sign In</button>
-            <button className="bg-red-500 px-6 py-2 rounded text-white">Sign Up</button>
-        </div>
+      <Link to="/">
+        <h1 className="text-4xl font-bold text-red-500 cursor-pointer">
+          NETFLIX
+        </h1>
+      </Link>
+      <div>
+        <Link to="/login">
+          <button className="text-white mr-4">Sign In</button>
+        </Link>
+        <Link to="/signup">
+          
+        <button className="bg-red-500 px-6 py-2 rounded text-white">
+          Sign Up
+        </button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
